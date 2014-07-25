452583ae17d5a0d0ad398f3632debd1453e82253
Vincent Nëel <vincent@vneel.com>
2014-07-25 20:40:07 +0200

    Removed example for FileServeHandler

--- handler.go
+++ handler.go
@@ -6,18 +6,6 @@ import "net/http"
 type HandlerFunction func(*RequestContext) error
 
 // FileServeHandler registers the specified folder to be served on the specified route.
-// For example:
-//
-//		p := pi.New()
-//		p.Route("/image").Get(pi.FileServeHandler("/var/www/public/images"))
-//		p.ListenAndServe(":8080")
-//
-// Or:
-//
-//		p := pi.New()
-//		p.Route("/image/{file}").Get(pi.FileServeHandler("/var/www/public/images"))
-//		p.ListenAndServe(":8080")
-//
 func FileServeHandler(path string) HandlerFunction {
 	return func(c *RequestContext) error {
 		file := c.R.URL.Query().Get(":file")
