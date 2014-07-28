44be5fe518f1caf4bea7aae479e5390501922e96
Vincent Nëel <vincent@vneel.com>
2014-07-28 17:34:26 +0200

    Fix stupid mistake

--- requestContext.go
+++ requestContext.go
@@ -218,28 +218,28 @@ func (c *RequestContext) GetFileHeaders(key string) ([]*multipart.FileHeader, er
 
 // GetHeader returns the first value of the header associated with the given key.
 func (c *RequestContext) GetHeader(key string) string {
-	return c.R.Header.Get(key)
+	return c.W.Header.Get(key)
 }
 
 // SetHeader sets the header entries associated with key to
 // the single element value.  It replaces any existing
 // values associated with key.
 func (c *RequestContext) SetHeader(key, value string) {
-	c.R.Header.Set(key, value)
+	c.W.Header.Set(key, value)
 }
 
 // AddHeader adds the key, value pair to the header.
 // It appends to any existing values associated with key.
 func (c *RequestContext) AddHeader(key, value string) {
-	c.R.Header.Add(key, value)
+	c.W.Header.Add(key, value)
 }
 
 // DeleteHeader deletes the values associated with key.
 func (c *RequestContext) DeleteHeader(key string) {
-	c.R.Header.Del(key)
+	c.W.Header.Del(key)
 }
 
 // GetHeaders returns the array of values associated with the key.
 func (c *RequestContext) GetHeaders(key string) []string {
-	return c.R.Header[key]
+	return c.W.Header[key]
 }
