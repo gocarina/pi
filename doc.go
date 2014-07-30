18bae76209373a3b3112f24dc1af45a336552d82
Vincent Nëel <vincent@vneel.com>
2014-07-30 22:10:44 +0200

    Updated doc

--- doc.go
+++ doc.go
@@ -5,19 +5,30 @@ It has been written in top of the router from the pat toolkit (http://gorillatoo
 
 Simple example:
 
+		import "github.com/gocarina/pi"
+
 		func main() {
 			p := pi.New()
 			p.Router("/",
 
 				p.Route("/products",
-						p.Route("/{id}").Delete(DeleteProductHandler).Put(EditProductHandler).BeforeFunc(AuthorizeUser)).
+
+						// "/products/{id}"
+						p.Route("/{id}").
+						Delete(DeleteProductHandler).
+						Put(EditProductHandler).
+						BeforeFunc(AuthorizeUser)).
+
+				// "/products"
 				Get(GetProductsHandler).
 				Post(AddProductHandler))
+
+				p.ListenAndServe(":8080")
 		}
 
 This example registers two different routes :
- - "/products" with two methods: GetProductsHandler (GET) and AddProductHandler (POST)
- - "/products/{id}" with also two methods: DeleteProductHandler (GET) and EditProductHandler (PUT)
+- "/products" with two methods: GetProductsHandler (GET) and AddProductHandler (POST)
+- "/products/{id}" with also two methods: DeleteProductHandler (GET) and EditProductHandler (PUT)
 
 There is also an interceptor Before in the route "/products/{id}", that means that the function AuthorizeUser is called before calling either
 DeleteProductHandler or EditProductHandler.
@@ -25,10 +36,10 @@ DeleteProductHandler or EditProductHandler.
 If there was interceptors on the route "/products", it would also apply for the child routes.
 There is 3 kind of interceptor:
 
- - Before: called before the request is handled by the handler. If there is an error in a Before interceptor, the request flow is stopped and
-   Error interceptors are called.
- - After: called after the request has been handled by the handled. Errors are ignored (print to the error output).
- - Error: called when an error occurs in any Before interceptor or in the request handler. Errors are completely ignored.
+- Before: called before the request is handled by the handler. If there is an error in a Before interceptor, the request flow is stopped and
+ Error interceptors are called.
+- After: called after the request has been handled by the handled. Errors are ignored (print to the error output).
+- Error: called when an error occurs in any Before interceptor or in the request handler. Errors are completely ignored.
 
 Interceptors and Handler are both HandlerFunction.
 Here is a typical example of HandlerFunction:
