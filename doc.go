8a563616e4b9b2d71123f00c6fcd6b852a214a1f
Vincent Nëel <vincent@vneel.com>
2014-07-30 22:14:34 +0200

    Updated doc

--- doc.go
+++ doc.go
@@ -1,7 +1,7 @@
 /*
 Package pi is a powerful toolkit to fasten the writing of web services in Go.
-
 It has been written in top of the router from the pat toolkit (http://gorillatoolkit.org/pkg/pat).
+
 
 Simple example:
 
@@ -26,8 +26,11 @@ Simple example:
 				p.ListenAndServe(":8080")
 		}
 
+
 This example registers two different routes :
+
 - "/products" with two methods: GetProductsHandler (GET) and AddProductHandler (POST)
+
 - "/products/{id}" with also two methods: DeleteProductHandler (GET) and EditProductHandler (PUT)
 
 There is also an interceptor Before in the route "/products/{id}", that means that the function AuthorizeUser is called before calling either
@@ -36,12 +39,14 @@ DeleteProductHandler or EditProductHandler.
 If there was interceptors on the route "/products", it would also apply for the child routes.
 There is 3 kind of interceptor:
 
+
 - Before: called before the request is handled by the handler. If there is an error in a Before interceptor, the request flow is stopped and
 Error interceptors are called.
 
 - After: called after the request has been handled by the handled. Errors are ignored (print to the error output).
 
 - Error: called when an error occurs in any Before interceptor or in the request handler. Errors are completely ignored.
+
 
 Interceptors and Handlers are both HandlerFunction.
 Here is a typical example of HandlerFunction:
