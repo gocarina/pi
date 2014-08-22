b250d29995e69ed881fc0998230384eae6af9346
Vincent Nëel <vincent@vneel.com>
2014-08-22 23:27:13 +0200

    Changed the way the error works: its interface now, so one can create custom errors in a more elegant way

--- error.go
+++ error.go
@@ -5,34 +5,62 @@ import (
 )
 
 // HTTPError represents a HTTP Error.
-type HTTPError struct {
-	StatusCode    int `json:"statusCode"`
-	Err           error
-	ErrorTemplate string
-	ContentType   string
+type HTTPError interface {
+	error
+	ContentType() string
+	StatusCode() int
 }
 
-// Error implements the Error interface.
-func (h HTTPError) Error() string {
-	return fmt.Sprintf(h.ErrorTemplate, int64(h.StatusCode), h.Err.Error())
+type _JSONError struct {
+	statusCode int
+	err        string
+	template   string
+}
+
+func (error _JSONError) Error() string {
+	return fmt.Sprintf(error.template, int(error.statusCode), error.err)
+}
+
+func (error _JSONError) StatusCode() int {
+	return error.statusCode
+}
+
+func (error _JSONError) ContentType() string {
+	return "application/json; charset=UTF-8"
+}
+
+type _XMLError struct {
+	statusCode int
+	err        string
+	template   string
+}
+
+func (error _XMLError) Error() string {
+	return fmt.Sprintf(error.template, int(error.statusCode), error.err)
+}
+
+func (error _XMLError) ContentType() string {
+	return "application/xml; charset=UTF-8"
+}
+
+func (error _XMLError) StatusCode() int {
+	return error.statusCode
 }
 
 // NewError returns a new HTTPError, and outputs it as JSON.
 func NewError(statusCode int, err error) HTTPError {
-	return HTTPError{
-		StatusCode:    statusCode,
-		Err:           err,
-		ErrorTemplate: `{"errorCode": %d, "errorMessage": "%s"}`,
-		ContentType:   "application/json; charset=UTF-8",
+	return _JSONError{
+		statusCode: statusCode,
+		err:        err.Error(),
+		template:   `{"errorCode": %d, "errorMessage": "%s"}`,
 	}
 }
 
 // NewXMLError returns a new HTTPError, and outputs it as XML.
 func NewXMLError(statusCode int, err error) HTTPError {
-	return HTTPError{
-		StatusCode:    statusCode,
-		Err:           err,
-		ErrorTemplate: `<error code="%d">%s</error>`,
-		ContentType:   "application/xml; charset=UTF-8",
+	return _XMLError{
+		statusCode: statusCode,
+		err:        err.Error(),
+		template:   `<error code="%d">%s</error>`,
 	}
 }
